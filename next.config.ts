5497d753eddcc1f10c9aed104c70e5ae69a86737
TomashKej <168352007+TomashKej@users.noreply.github.com>
2026-02-20 23:10:47 +0000

    Initial portfolio upload
    Add gitignore and remove VS cache files

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    output: 'export',
-    basePath: '/TomaszDev.github.io',
 };
 
 export default nextConfig;
